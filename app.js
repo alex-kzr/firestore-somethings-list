110bd8b9186ae51f602ace84395364af86f736a0
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-01-23 22:03:36 +1000

    Add li render to list

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+const somethingList = document.querySelector("#something-list");
+
+// create element and render something
+function renderSomething(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let note = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    note.textContent = doc.data().name;
+
+    li.appendChild(name);
+    li.appendChild(note);
+
+    somethingList.appendChild(li);
+}
+
 db.collection('somethings').get().then((snapshot) => {
     //console.log(snapshot.docs);
     snapshot.docs.forEach(doc => {
-        console.log(doc.data());
+        renderSomething(doc);
     });
 });
